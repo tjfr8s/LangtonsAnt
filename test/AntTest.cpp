25afbf7a2bd9ceb083bf98b79e0024b4adf9c37e
tyler <tjfr8s@gmail.com>
2018-06-28 10:18:25 -0700

    Implement Ant constructor that takes integer row and column arguments. Implement constructor test in AntTest.cpp.

--- test/AntTest.cpp
+++ test/AntTest.cpp
@@ -1,7 +1,16 @@
 #include <iostream>
+#include <cstdio>
 #include "../Ant.hpp"
 
 int main()
 {
-    Ant testAnt(1, 2);
+    // Test Ant constructor
+    Ant ant1(1, 2);
+
+    printf("Ant row: %i \n"
+           "Ant col: %i \n"
+           "Ant orientation: %i \n",
+            ant1.getRow(),
+            ant1.getCol(),
+            ant1.getOrientation());
 }
