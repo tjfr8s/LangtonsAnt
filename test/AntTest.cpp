cccdfa984211297f84b26fcf6753c34b3dc19a66
tyler <tjfr8s@gmail.com>
2018-06-28 13:23:36 -0700

    Implement tests Ant::move() edge cases.

--- test/AntTest.cpp
+++ test/AntTest.cpp
@@ -66,5 +66,76 @@ int main()
     printf("After move up row: "
            "%i, col: %i\n",
             ant2.getRow(), ant2.getCol());
+
+
+    ant2.setCol(0);
+    ant2.setRow(0);
+    ant2.setOrientation(Ant::LEFT);
+    printf("\nCurrent position is row: %i, "
+           "col: %i, orientation: %i",
+            ant2.getRow(),
+            ant2.getCol(),
+            ant2.getOrientation());
+
+    printf("\nReturn value of Ant::move(): %s",
+            (ant2.move(3, 3) ? "true\n" : "false\n"));
+    printf("Position after move is row: %i, "
+           "col: %i, orientation: %i\n",
+            ant2.getRow(),
+            ant2.getCol(),
+            ant2.getOrientation());
+
+
+    ant2.setCol(0);
+    ant2.setRow(0);
+    ant2.setOrientation(Ant::UP);
+    printf("\nCurrent position is row: %i, "
+           "col: %i, orientation: %i",
+            ant2.getRow(),
+            ant2.getCol(),
+            ant2.getOrientation());
+
+    printf("\nReturn value of Ant::move(): %s",
+            (ant2.move(3, 3) ? "true\n" : "false\n"));
+    printf("Position after move is row: %i, "
+           "col: %i, orientation: %i\n",
+            ant2.getRow(),
+            ant2.getCol(),
+            ant2.getOrientation());
            
+    
+    ant2.setCol(3);
+    ant2.setRow(3);
+    ant2.setOrientation(Ant::RIGHT);
+    printf("\nCurrent position is row: %i, "
+           "col: %i, orientation: %i",
+            ant2.getRow(),
+            ant2.getCol(),
+            ant2.getOrientation());
+
+    printf("\nReturn value of Ant::move(): %s",
+            (ant2.move(3, 3) ? "true\n" : "false\n"));
+    printf("Position after move is row: %i, "
+           "col: %i, orientation: %i\n",
+            ant2.getRow(),
+            ant2.getCol(),
+            ant2.getOrientation());
+
+
+    ant2.setCol(3);
+    ant2.setRow(3);
+    ant2.setOrientation(Ant::DOWN);
+    printf("\nCurrent position is row: %i, "
+           "col: %i, orientation: %i",
+            ant2.getRow(),
+            ant2.getCol(),
+            ant2.getOrientation());
+
+    printf("\nReturn value of Ant::move(): %s",
+            (ant2.move(3, 3) ? "true\n" : "false\n"));
+    printf("Position after move is row: %i, "
+           "col: %i, orientation: %i\n",
+            ant2.getRow(),
+            ant2.getCol(),
+            ant2.getOrientation());
 }
