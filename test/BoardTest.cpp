f94c54c70334cbe76202a74b39aee7d22bb9dc7a
tyler <tjfr8s@gmail.com>
2018-06-29 18:26:17 -0700

    Add console delay after printing board.

--- test/BoardTest.cpp
+++ test/BoardTest.cpp
@@ -1,12 +1,19 @@
 #include <iostream>
 #include <cstdio>
+#include <unistd.h>
 #include "../Board.hpp"
 #include "../Space.hpp"
 
 
 int main()
 {
-    Board board1(15, 16);
-    board1.print();
+    Board board1(50, 90);
+    for(int i = 0; i < 23000; i++)
+    {
+        
+        board1.print();
+        board1.moveAnt();
+        usleep(1000000);
+    }
     return 0;
 }
