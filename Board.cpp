3177439734cd860dcd9a3546ca86ebf9cb65e054
tyler <tjfr8s@gmail.com>
2018-07-01 10:03:40 -0700

    Begin adding comments to Board.cpp.

--- Board.cpp
+++ Board.cpp
@@ -1,11 +1,39 @@
 /************************************************************
- * Author:
- * Date:
- * Description:
+ * Author: Tyler Freitas
+ * Date: 06/30/2018
+ * Description: This class represents a board on which to 
+ * simulate Langton's Ant. Board has the following data:
+ * - m_Rows: the number of rows on the board.
+ * - m_Cols: the number of columns on the board.
+ * - m_board: a 2D dynamic array of Space objects that 
+ *   represents the board.
+ * - m_ant: represents the ant on the board.
+ *
+ * The board's constructor initializes the number of rows
+ * and columns to the passed values and dynamically allocates
+ * space for a 2D array of space objects with those dimensions.
+ * It also initializes the position of the ant to 
+ * (numRows/2, numCols/2) putting it near the center of the board.
+ *
+ * The antLeave(int row, int col) member function updates the 
+ * space at the passed location to not contain the ant. The
+ * antEnter(int row, int col) member function updates the space
+ * at the passed location to contain the ant.
+ *
+ * The moveAnt() member function causes the ant to move forward 
+ * a single space and turn left if the space it is entering is 
+ * black and right if it is white. It also calls antEnter()
+ * and antLeave() on the appropriate board locations to update
+ * them.
 ************************************************************/
 #include "Board.hpp"
 #include "Space.hpp"
 
+/************************************************************
+ * Description: 
+ * Preconditions:
+ *      Parameters: 
+************************************************************/
 Board::Board(int numRows, int numCols) 
     : m_numRows(numRows), 
       m_numCols(numCols),
